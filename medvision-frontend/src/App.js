da78df8156a93ca10990e90fe76cb8cf45f17bda
Nibalab <abdalbakinibal@gmail.com>
2024-08-29 01:04:20 +0300

    building the signup Page

--- medvision-frontend/src/App.js
+++ medvision-frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignupPage from '../src/pages/SignupPage/SignupPage';
+// import DoctorSignup from './DoctorSignup';
+// import PatientSignup from './PatientSignup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route path="/register/doctor" element={<DoctorSignup />} />
+        <Route path="/register/patient" element={<PatientSignup />} /> */}
+        <Route path="/" element={<SignupPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ medvision-frontend/src/pages/SignupPage/SignupPage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './SignupPage.css';
+import logo from '../../Images/Logo.png';
+
+const SignupPage = () => {
+    const navigate = useNavigate();
+  
+    const handleDoctorSignup = () => {
+      navigate('/register/doctor');
+    };
+  
+    const handlePatientSignup = () => {
+      navigate('/register/patient');
+    };
+  
+    return (
+      <div className="signup-container">
+        <div className="left-side">
+          <img src={logo} alt="MedVision Logo" className="logo" />
+        </div>
+        <div className="right-side">
+          <h2>Please SignUp</h2>
+          <button className="signup-button doctor" onClick={handleDoctorSignup}>
+            Sign up as a doctor
+          </button>
+          <button className="signup-button patient" onClick={handlePatientSignup}>
+            Sign up as a patient
+          </button>
+        </div>
+      </div>
+    );
+  };
+  
+  export default SignupPage;
