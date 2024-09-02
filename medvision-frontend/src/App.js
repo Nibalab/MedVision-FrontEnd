5c3b4a1fb7665e5c4821475b28e26bfe82d5f124
Nibalab <abdalbakinibal@gmail.com>
2024-09-02 22:31:29 +0300

    fixing the style of the login page

--- medvision-frontend/src/App.js
+++ medvision-frontend/src/App.js
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignupPage from '../src/pages/SignupPage/SignupPage';
 import DoctorSignup from '../src/pages/DoctorSignUp/DoctorSignUp';
 import PatientSignup from '../src/pages/PatientSignUp/PatientSignUp';
+import LoginPage from '../src/pages/Login/Login'; 
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<LoginPage />} /> 
         <Route path="/register/doctor" element={<DoctorSignup />} />
         <Route path="/register/patient" element={<PatientSignup />} />
-        <Route path="/" element={<SignupPage />} />
+        <Route path="/register" element={<SignupPage />} /> 
       </Routes>
     </Router>
   );
---
+++ medvision-frontend/src/pages/Login/Login.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Login.css'; 
+import logo from '../../Images/Logo.png';
+import { useNavigate } from 'react-router-dom';
+
+const LoginPage = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate();
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/login', formData);
+      const { role } = response.data.user;
+      if (role === 'doctor') {
+        navigate('/doctor-dashboard');
+      } else if (role === 'patient') {
+        navigate('/patient-dashboard');
+      } else if (role === 'admin') {
+        navigate('/admin-dashboard');
+      }
+    } catch (error) {
+      console.error('Error logging in:', error);
+      setErrorMessage('Login failed. Please check your credentials and try again.');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-left-side">
+        <form className="login-box" onSubmit={handleSubmit}>
+          <h2>Login</h2>
+          <input 
+            type="email" 
+            name="email" 
+            placeholder="Enter your email" 
+            value={formData.email} 
+            onChange={handleInputChange} 
+            required 
+          />
+          <input 
+            type="password" 
+            name="password" 
+            placeholder="Enter your password" 
+            value={formData.password} 
+            onChange={handleInputChange} 
+            required 
+          />
+          <div className="login-options">
+            <a href="/forgot-password" className="forgot-password-link">Forgot Password?</a>
+          </div>
+          <div className="login-terms-wrapper">
+    <input type="checkbox" className="terms-checkbox" id="terms-checkbox" required />
+    <label htmlFor="terms-checkbox">
+        Accept the <a href="/terms">Terms and Conditions</a> and <a href="/privacy">Privacy Policy</a>
+    </label>
+</div>
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
+          <button type="submit" className="login-button">Login</button>
+          <p>Don't have an account? <a href="/register">Sign up</a></p>
+        </form>
+      </div>
+      <div className="login-right-side">
+        <img src={logo} alt="MedVision Logo" className="login-logo" />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
