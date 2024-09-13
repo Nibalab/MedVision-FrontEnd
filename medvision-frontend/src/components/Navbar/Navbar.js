ff99303cc7049631577478df5fc9e83be17d3087
Nibalab <abdalbakinibal@gmail.com>
2024-09-13 17:39:48 +0300

    update the Navbar

--- medvision-frontend/src/components/Navbar/Navbar.js
+++ medvision-frontend/src/components/Navbar/Navbar.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../../Images/Logo2.png';
 import { FaSignOutAlt } from 'react-icons/fa';
+import axios from 'axios';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    const token = localStorage.getItem('token');
+
+    // Call the backend API to perform logout
+    axios.post('http://127.0.0.1:8000/api/logout', {}, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then(() => {
+      localStorage.removeItem('token'); // Remove the token from local storage
+      navigate('/'); // Redirect to the login or home page after logout
+    })
+    .catch((error) => {
+      console.error('Error logging out:', error);
+    });
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
@@ -46,7 +67,7 @@ const Navbar = () => {
       </ul>
       <div className="navbar-logout">
         <FaSignOutAlt />
-        <button onClick={() => alert('Logging out...')}>Logout</button>
+        <button onClick={handleLogout}>Logout</button>
       </div>
     </nav>
   );
