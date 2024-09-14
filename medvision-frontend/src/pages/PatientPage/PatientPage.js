1054b9cf370e314c8798db43a67b25a8e98f1938
Nibalab <abdalbakinibal@gmail.com>
2024-09-15 01:53:49 +0300

    updating the Patient page

--- medvision-frontend/src/pages/PatientPage/PatientPage.js
+++ medvision-frontend/src/pages/PatientPage/PatientPage.js
@@ -49,7 +49,6 @@ const PatientPage = () => {
     }
   };
 
-  // Function to download the patient report from the database
   const handleDownloadReport = async (patientId) => {
     try {
       const response = await axios.get(`http://127.0.0.1:8000/api/patients/${patientId}/download-report`, {
