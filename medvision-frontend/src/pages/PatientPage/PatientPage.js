8a959b99f38696277b0ad52c64f1045e8da40b64
Nibalab <abdalbakinibal@gmail.com>
2024-09-15 01:54:00 +0300

    updating the Patient page

--- medvision-frontend/src/pages/PatientPage/PatientPage.js
+++ medvision-frontend/src/pages/PatientPage/PatientPage.js
@@ -61,7 +61,7 @@ const PatientPage = () => {
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement('a');
       link.href = url;
-      link.setAttribute('download', `patient_${patientId}_report.pdf`); // Specify file name
+      link.setAttribute('download', `patient_${patientId}_report.pdf`); 
       document.body.appendChild(link);
       link.click();
       link.remove();
