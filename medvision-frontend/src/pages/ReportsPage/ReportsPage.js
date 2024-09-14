17b2191a730c653acaa687705f2ef9f3745a779c
Nibalab <abdalbakinibal@gmail.com>
2024-09-15 02:00:31 +0300

    updating the Report page

--- medvision-frontend/src/pages/ReportsPage/ReportsPage.js
+++ medvision-frontend/src/pages/ReportsPage/ReportsPage.js
@@ -25,7 +25,7 @@ const ReportsPage = () => {
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement('a');
       link.href = url;
-      link.setAttribute('download', `report_${reportId}.pdf`); // Specify file name
+      link.setAttribute('download', `report_${reportId}.pdf`); 
       document.body.appendChild(link);
       link.click();
       link.remove();
